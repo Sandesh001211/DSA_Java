dac21257eb382adf2ffae6a500a8bec6922b19bb
Sandesh <dongaonkarsandesh001@gmail.com>
2026-03-02 20:54:43 +0530

    added logic of string compression

---
+++ string/Compression.java
@@ -0,0 +1,27 @@
+package string;
+
+class Compression {
+    public static String compress_string(String str) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            int count = 1;
+
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count);
+            }
+
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbddd";
+        System.out.println(compress_string(str));
+    }
+
+}
