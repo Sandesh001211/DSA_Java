7eff92a5e51743088984267bc915f2244f494f07
Sandesh <dongaonkarsandesh001@gmail.com>
2026-03-04 15:11:25 +0530

    add edge cases and optimizationof Bubble Sort

--- arrays/BubbleSort.java
+++ arrays/BubbleSort.java
@@ -2,24 +2,37 @@ package arrays;
 
 class BubbleSort {
     public static void bubble_sort(int num[]) {
+
         for (int i = 0; i < num.length - 1; i++) {
+
+            boolean swapped = false;
+
             for (int j = 0; j < num.length - 1 - i; j++) {
+
                 if (num[j] > num[j + 1]) {
                     int temp = num[j];
                     num[j] = num[j + 1];
                     num[j + 1] = temp;
+                    swapped = true;
                 }
             }
 
+            if (!swapped) {
+                if (i == 0) {
+                    System.out.println("Array already sorted");
+                }
+                break;
+            }
         }
     }
 
     public static void main(String[] args) {
-        int num[] = { 5, 3, 100, 22, 6, 8 };
+        int num[] = {1,6,5,4,8,7};
+
         bubble_sort(num);
+
         for (int i : num) {
             System.out.print(i + " ");
         }
-
     }
 }
