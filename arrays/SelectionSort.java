3d4038a077e05bee98f2809c3c397d673519950a
Sandesh <dongaonkarsandesh001@gmail.com>
2026-03-06 23:43:51 +0530

    Fix loop

--- arrays/SelectionSort.java
+++ arrays/SelectionSort.java
@@ -6,12 +6,12 @@ public class SelectionSort {
             int minPos = i;
 
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[minPos] < arr[j]) {
+                if (arr[minPos] > arr[j]) {
                     minPos = j;
                 }
             }
 
-            // swap
+            // swap 
             int temp = arr[minPos];
             arr[minPos] = arr[i];
             arr[i] = temp;
@@ -22,7 +22,7 @@ public class SelectionSort {
         int arr[] = { 5, 4, 3, 2, 1 };
         selectionSort(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 
