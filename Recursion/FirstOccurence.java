41aa2f7249a3719bc6d8e05522d7b7ca1ac0f846
Sandesh <dongaonkarsandesh001@gmail.com>
2026-04-05 23:13:43 +0530

    Implements program to ind first occurence

---
+++ Recursion/FirstOccurence.java
@@ -0,0 +1,19 @@
+class FirstOccurence
+{
+    public static int findFirstOccurence(int arr[] , int key , int i )
+    {
+        if (i==arr.length)
+        {
+            return -1;
+        }
+        if (arr[i]==key)
+        {
+            return i;
+        }
+         return findFirstOccurence(arr, key, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,5,5};
+        System.out.println(findFirstOccurence(arr, 5, 0));
+    }
+}
